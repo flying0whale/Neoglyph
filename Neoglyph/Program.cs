a39a96cff040b989e9fc4114b9ac85227b4b9360
flying0whale <maks.manin.04@gmail.com>
2025-04-12 17:29:06 +0300

    Added GlyphWriter.cs

--- Neoglyph/Program.cs
+++ Neoglyph/Program.cs
@@ -1,8 +1,16 @@
-﻿using Neoglyph.Glyphs;
+﻿using Neoglyph.Dictionary;
+using Neoglyph.Glyph;
+using Neoglyph.Graphics;
 
-const string PATH = "../../../Dictionary/english.json";
-var glyphs = GlyphDictionary.FromFile(PATH);
+var canvas = new SvgCanvas(20, 50, 50, 
+				 Colors.White,
+				 Colors.Black);
+canvas.DrawGrid();
 
-foreach (var pair in glyphs) {
-	Console.WriteLine($"{pair.Key}: {pair.Value}");
-}
+var glyphs = GlyphDictionary.FromFile("../../../Dictionary/english.json");
+
+var glyphWriter = new GlyphWriter(glyphs, canvas);
+
+glyphWriter.DrawText("hello world i am neoglyph a cryptographic program", new Cell(1, 1));
+
+canvas.SaveToFile("../../../canvas.svg");
